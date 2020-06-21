8435a5be9b9fc47ace9a1f6b6aaf4d112a40afef
gavrielhas <65192265+gavrielhas@users.noreply.github.com>
2020-06-22 01:39:09 +0300

    fixed closing on clicking togglebar

--- src/components/headerComponents/Example.js
+++ src/components/headerComponents/Example.js
@@ -6,16 +6,50 @@ export default class Example extends React.Component {
   showSettings(event) {
     event.preventDefault();
   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false,
+    };
+  }
+
+  // This keeps your state in sync with the opening/closing of the menu
+  // via the default means, e.g. clicking the X, pressing the ESC key etc.
+  handleStateChange(state) {
+    this.setState({ menuOpen: state.isOpen });
+  }
+
+  // This can be used to close the menu, e.g. when a user clicks a menu item
+  closeMenu() {
+    this.setState({ menuOpen: false });
+  }
+
+  // This can be used to toggle the menu, e.g. when using a custom icon
+  // Tip: You probably want to hide either/both default icons if using a custom icon
+  // See https://github.com/negomi/react-burger-menu#custom-icons
+  toggleMenu() {
+    this.setState((state) => ({ menuOpen: !state.menuOpen }));
+  }
 
   render() {
     // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
     return (
-      <div className="justify-content-right">
-        <Menu right>
-          <Link id="about" className="menu-item text-capitalize" to="/">
+      <div>
+        <Menu
+          isOpen={this.state.menuOpen}
+          onStateChange={(state) => this.handleStateChange(state)}
+          right
+        >
+          <Link
+            onClick={() => this.closeMenu()}
+            id="about"
+            className="menu-item text-capitalize"
+            to="/"
+          >
             home
           </Link>
           <Link
+            onClick={() => this.closeMenu()}
             className="menu-item text-capitalize"
             id="home"
             to="/ProductList"
@@ -23,10 +57,20 @@ export default class Example extends React.Component {
             product list
           </Link>
 
-          <Link id="contact" className="menu-item text-capitalize" to="/">
+          <Link
+            onClick={() => this.closeMenu()}
+            id="contact"
+            className="menu-item text-capitalize"
+            to="/"
+          >
             Contact
           </Link>
-          <Link id="cart" className="menu-item text-capitalize" to="/cart">
+          <Link
+            onClick={() => this.closeMenu()}
+            id="cart"
+            className="menu-item text-capitalize"
+            to="/cart"
+          >
             cart
           </Link>
         </Menu>
