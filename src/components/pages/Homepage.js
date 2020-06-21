bc23a9bdeb18134118f463be20bf46806bb90360
gavrielhas <65192265+gavrielhas@users.noreply.github.com>
2020-06-22 00:44:00 +0300

    added picture and button to homepage

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -1,24 +1,39 @@
 import React from "react";
-import nuts from "../headerComponents/nuts-1.jpg";
+import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
-    <div className="container-fluid">
+    <React.Fragment>
       <h1>Home Page Content</h1>
-      <div className="backgroundImage"></div>
-      <h1>Try our nuts</h1>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
-    </div>
+      <section>
+        <div className="BorderImg">
+          <div className=" img-fluid">
+            <h1 className="img-text">
+              <Link to="/ProductList">
+                <button className="ButtonImg">To our nuts</button>
+              </Link>
+            </h1>
+          </div>
+        </div>
+
+        <h1 className="">Try our nuts</h1>
+        <p>
+          Our nuts is well selected, descripiton about the products Lorem Ipsum
+          is simply dummy text of the printing and typesetting industry. Lorem
+          Ipsum has been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book. It has survived not only five centuries,
+          but also the leap into electronic typesetting, remaining essentially
+          unchanged. It was popularised in the 1960s with the release of
+          Letraset sheets containing Lorem Ipsum passages, and more recently
+          with desktop publishing software like Aldus PageMaker including
+          versions of Lorem Ipsum.
+        </p>
+      </section>
+      <section>
+        <div></div>
+      </section>
+    </React.Fragment>
   );
 }
 
